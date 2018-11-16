c171e550c0930d2111ba8260b16a215e81bc70f3
hamdi <javs_shao@163.com>
2018-11-16 20:16:10 +0800

    视图函数_生成二维码

---
+++ Django_erweima/tools/views.py
@@ -0,0 +1,17 @@
+import qrcode
+from django.http import HttpResponse
+from django.utils.six import  BytesIO
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def generate_qrcode(request, data):
+    img = qrcode.make(data)
+
+    buf = BytesIO()
+    img.save(buf)
+    image_stream = buf.getvalue()
+
+    response = HttpResponse(image_stream, content_type='image/png')
+    return response
